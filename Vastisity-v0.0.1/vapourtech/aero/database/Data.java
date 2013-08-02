12647917ce4abaa48f90d385da33e6cdbafc5df5
SkyTheVenture <maybeanother98@gmail.com>
2013-08-02 19:22:25 +0100

    Uploaded Data.java
    The data files.

---
+++ Vastisity-v0.0.1/vapourtech/aero/database/Data.java
@@ -0,0 +1,13 @@
+package vapour.aero.database;
+
+public class Data {
+  
+	public String v;
+	public String data;
+	
+	public Data(String variable, String data){
+		v = variable;
+		this.data = data;
+	}
+
+}
