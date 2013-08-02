6e1ce000c85efd107bb3466a25448933c586dc00
SkyTheVenture <maybeanother98@gmail.com>
2013-08-02 19:41:58 +0100

    Create Data.java

---
+++ Vastisity-v0.0.2/vapourtech/aero/database/datatypes/Data.java
@@ -0,0 +1,13 @@
+package vapour.aero.database.datatypes;
+
+public class Data {
+  
+	public String v;
+	public Object data;
+	
+	public Data(String variable, Object data){
+		v = variable;
+		this.data = data;
+	}
+
+}
