f47c21d014cc673f67bb8914f7784c244a1ee9cc
SkyTheVenture <maybeanother98@gmail.com>
2013-08-02 19:23:50 +0100

    Update Vastisity.java

--- Vastisity-v0.0.1/vapourtech/aero/database/Vastisity.java
+++ Vastisity-v0.0.1/vapourtech/aero/database/Vastisity.java
@@ -1,6 +1,8 @@
 package vapour.aero.database;
 
 public class Vastisity {
+	
+	public static String version = "0.0.1 (V1)";
   
 	public static Database loadDatabase(String file) throws Exception{
 		Database db = new Database();
